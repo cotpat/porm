f2bda47e7e4878538ebcd71abdacd894017f1eb5
Patrick <patrick@ptct.ch>
2024-04-10 13:37:32 +0200

    add: try to open connection :S

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
 require_once 'vendor/autoload.php';
+use Cotpat\Porm\Connection;
+use mysqli;
 
-$class = new \Cotpat\Porm\Connection();
+$connection = new Connection(
+  "127.0.0.1",
+  "root",
+  "root",
+  "test",
+  "3306"
+);
 
 echo file_get_contents("html/main.html");
