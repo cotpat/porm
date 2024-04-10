ed2a6a559e28de09ce0d330e82c09ac7b922e170
Patrick <patrick@ptct.ch>
2024-04-10 10:53:47 +0200

    add: DB connection attributes

--- src/Connection.php
+++ src/Connection.php
@@ -4,4 +4,14 @@ namespace Cotpat\Porm;
 
 class Connection {
 
+    /**
+     * Data required to connect to MySQL server
+     */
+    private $host = '';
+    private $username = '';
+    private $password = '';
+    private $dbName = '';
+    private $port = '';
+    private $socket = '';
+    private $mysqli;
 }
