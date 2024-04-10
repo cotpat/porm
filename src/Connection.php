bbdfe794eb2ccccc835080f7534c38e68b9fe594
Patrick <patrick@ptct.ch>
2024-04-10 11:27:17 +0200

    add: constructor add: methods

--- src/Connection.php
+++ src/Connection.php
@@ -2,6 +2,8 @@
 
 namespace Cotpat\Porm;
 
+use mysqli;
+
 class Connection {
 
     /**
@@ -14,4 +16,68 @@ class Connection {
     private $port = '';
     private $socket = '';
     private $mysqli;
+
+    /**
+     * Constructor for DB connection
+     * Automatically attemps to open connection
+     *
+     * @param string $host
+     * @param string $username
+     * @param string $password
+     * @param string $dbName
+     * @param string $port
+     * @param string $socket
+     * 
+     * @return void
+     */
+    public function __construct(
+        $host,
+        $username,
+        $password,
+        $dbName,
+        $port,
+        $socket,
+    ) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbName = $dbName;
+        $this->port = $port;
+        $this->socket = $socket;
+
+        if (!$this->connect()) {
+            echo 'Connection error! Check your credentials, hosts, ports.';
+        }
+    }
+
+    /**
+     * Opens a connection.
+     * Called in the constructor.
+     *
+     * @return bool
+     */
+    private function connect()
+    {
+        $this->mysqli = new mysqli(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->dbName,
+            $this->port,
+            $this->socket
+        );
+
+        if ($this->mysqli->connect_error) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function disconnect()
+    {
+        if (isset($this->mysqli)) {
+            $this->mysqli->close();
+        }
+    }
 }
