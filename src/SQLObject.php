fe7c7999d8c06459bb2cc679ddc623871c09ad2d
Patrick <patrick@ptct.ch>
2024-04-10 13:36:48 +0200

    add: rudimentary SQLObject class

---
+++ src/SQLObject.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Cotpat\Porm;
+
+/**
+ * Represents an object mapping of an SQL table
+ */
+class SQLObject
+{
+  private $dbFields = [];
+
+  public function addRow(
+    $name,
+    $type,
+  ) {
+    
+  }
+}
